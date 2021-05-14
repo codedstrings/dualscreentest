af7841fbb81615c597a7df765e47c1847bbd78e4
Codedstrings <mridhulml@gmail.com>
2021-05-14 20:17:16 +0530

    Working scrollable player cards

--- lib/listitem.dart
+++ lib/listitem.dart
@@ -1,10 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:screen_change_test/screens/screen2.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-Widget listitem(String text)
-{
-return  GestureDetector(
-                      onTap: () {
+Widget listitem(String text) {
+  return GestureDetector(
+      onTap: () {
+        BuildContext context;
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => VideoPlayerScreen(),
+          ),
+        );
+        // Within the `FirstRoute` widget
+      },
+      child: Card(
+          elevation: 10,
+          child: SizedBox(
+            height: 300,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+              child: Center(
+                child: Text(
+                  text,
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          )));
+}
+
+Widget thumbnailitem(var vidurl) {
+  //var url = 'https://www.youtube.com/watch?v=V89BOZhJFlI';
+  YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: YoutubePlayer.convertUrlToId(vidurl),
+    flags: YoutubePlayerFlags(
+      autoPlay: true,
+      mute: true,
+    ),
+  );
+  return Card(
+      /*  onTap: () {
 
                          
                           BuildContext context;
@@ -14,21 +50,41 @@ return  GestureDetector(
                         );
                         // Within the `FirstRoute` widget
   
-                      },
-                      child: Card(
-                          elevation: 10,
-                          child: SizedBox(
-                            height: 300,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 8),
-                              child: Center(
-                                child: Text(
-                                  text,
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                              ),
-                            ),
-                          )));
+                      }, */
 
+      child: SizedBox(
+    height: 300,
+    child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+      child: Center(
+        child: YoutubePlayer(
+          controller: _controller,
+          liveUIColor: Colors.amber,
+        ),
+      ),
+    ),
+  ));
+}
+
+
+Widget thumbnail2(var vidurl){
+  
+  YoutubePlayerController _controller = YoutubePlayerController(
+    initialVideoId: YoutubePlayer.convertUrlToId(vidurl),
+    flags: YoutubePlayerFlags(
+      autoPlay: false,
+      mute: false,
+    ),
+  );
+
+  return Center(
+    child: Padding(padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 8),
+    child: YoutubePlayer(
+          controller: _controller,
+          liveUIColor: Colors.amber,
+        ),
+    )
+  );
+   
+ 
 }
--- lib/screens/screen1.dart
+++ lib/screens/screen1.dart
@@ -1,98 +1,47 @@
+//import 'package:add_thumbnail/add_thumbnail.dart';
 import 'package:flutter/material.dart';
 import 'package:screen_change_test/listitem.dart';
-import 'package:screen_change_test/screens/screen2.dart';
+//import 'package:screen_change_test/screens/screen2.dart';
+//import 'package:add_thumbnail/add_thumbnail.dart';
 
 //Thumbnail scroll class
 class ThumbnailScroll extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var passurl='https://www.youtube.com/watch?v=OpMyn7SdAWo';
+    var passurl1='https://www.youtube.com/watch?v=1MAuErvBtKA';
+    var passurl2='https://www.youtube.com/watch?v=gpkd6-8Akxo';
+    var passurl3='https://www.youtube.com/watch?v=uzkD5SeuwzM';
     return Scaffold(
         appBar: AppBar(backgroundColor: Colors.pink, title: Text("Thumbnails")), //appbar
 
         body: Container(
-          child: Padding(
+         child: Padding(
               padding: const EdgeInsets.all(10.0),
               child: ListView(
                 children: <Widget>[
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => VideoPlayerScreen(),  //route to second screen
-                          ),
-                        );
-                      },
-                      child: Card(
-                          elevation: 10,
-                          child: SizedBox(
-                            height: 300,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 8),
-                              child: Center(
-                                child: Text(
-                                  'Thumbnail 1',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                              ),
-                            ),
-                          ))),
-
+                  
                           //code for one card ends here, this card is inside a gesture detector which inturn is a children of listview widget
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => VideoPlayerScreen(),
-                          ),
-                        );
-                      }, //end of 1
-                      child: Card(
-                          elevation: 10,
-                          child: SizedBox(
-                            height: 300,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 8),
-                              child: Center(
-                                child: Text(
-                                  'Thumbnail 2',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                              ),
-                            ),
-                          ))),
-                          //code for second card ends here
-                  GestureDetector(
-                      onTap: () {
-                        Navigator.of(context).push(
-                          MaterialPageRoute(
-                            builder: (context) => VideoPlayerScreen(),
-                          ),
-                        );
-                      },
-                      child: Card(
-                          elevation: 10,
-                          child: SizedBox(
-                            height: 300,
-                            child: Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 10, horizontal: 8),
-                              child: Center(
-                                child: Text(
-                                  'Thumbnail 3',
-                                  style: TextStyle(fontSize: 18),
-                                ),
-                              ),
-                            ),
-                          ))),
-                          //code for third card ends
+                  
+                             thumbnail2(passurl),
+                             thumbnail2(passurl1),
+                             thumbnail2(passurl2),
+                             thumbnail2(passurl3),
+                            
 
-                          listitem('Thumbnail 4'), // to pass 4th and 5th thumbnail card as a function(not completed yet)
-                          listitem('Thumbnail 5'),
+                          
+                    
                 ],
-              )),
-        ));
+              )), 
+        )
+
+       
+        );
   }
 }
 //class ends
+
+
+  
+  
+
